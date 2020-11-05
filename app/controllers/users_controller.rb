173d6dd655c339f5666c10906e5197417c0f121e
kazuki <qiaobenk034@gmail.com>
2020-11-05 15:38:47 +0900

    asdfsaf

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+    # before_action :login_required
+  def index
+    if params[:team_id]
+      @team=Team.find(params[:team_id])
+      @users=@team.users
+    else
+      @users=User.all
+    end
+  end
+
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def edit
+    @user=User.find(params[:id])
+
+  end
+
+  def update
+    @user=User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:team_id,:email,:profile,:profile_image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   } 
   root to: "home#index"
 
+  resources :users 
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end

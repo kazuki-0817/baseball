4b8ef361cfef307edc589829dc94ee7e52d13b42
kazuki <qiaobenk034@gmail.com>
2020-11-05 15:11:48 +0900

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
     :sessions => 'users/sessions'   
   } 
   root to: "home#index"
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end

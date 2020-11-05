45d942d200bb5cdcb176fa6004cbfc35be7eef3e
kazuki <qiaobenk034@gmail.com>
2020-11-05 14:47:08 +0900

    adf

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :validatable
+
+  attachment :profile_image
+
+  validates :username, presence: true
 end

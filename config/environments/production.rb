9b46c09b2b6dc3e02687619fe92e5b2aa5d22198
kazuki <qiaobenk034@gmail.com>
2020-11-09 16:33:57 +0900

    11/9 devise con

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {  host: 'yahui55test@gmail.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "yahui55test@gmail.com",
+    :password => "1qaz0okm",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end

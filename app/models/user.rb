c7ad626763ff5155aa24b7202341acfec6efaf38
kazuki <qiaobenk034@gmail.com>
2020-11-09 15:47:36 +0900

    11/9 give

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # belongs_to :team,class_name: "Team",foreign_key: "team_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,:confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   attachment :profile_image
